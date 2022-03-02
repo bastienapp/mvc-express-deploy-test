de57d1d272e959e8de1af113ff3e14edeb74f4b3
bastienwcs <bastien.krettly@wildcodeschool.com>
2022-03-02 16:19:30 +0100

    select database

--- migrate.js
+++ migrate.js
@@ -4,18 +4,14 @@ const fs = require('fs');
 const mysql = require('mysql2/promise');
 
 const migrate = async () => {
-  const { DB_USER, DB_PASSWORD, DB_NAME } = process.env;
+  const { DB_HOST, DB_USER, DB_PASSWORD, DB_NAME } = process.env;
 
   const connection = await mysql.createPool({
-    host: 'http://srv-captain--database-db',
+    host: DB_HOST,
     user: DB_USER,
     password: DB_PASSWORD,
-    multipleStatements: true,
+    database: DB_NAME,
   });
-
-  await connection.query(`drop database if exists ${DB_NAME}`);
-  await connection.query(`create database ${DB_NAME}`);
-  await connection.query(`use ${DB_NAME}`);
 
   const sql = fs.readFileSync('./database.sql', 'utf8');
 
